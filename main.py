280230ba09a9435156aaad04a4cacc1e523041b7
Hadar-N <hadarnv1@gmail.com>
2025-02-06 18:56:15 +0800

    added streaming+buttons

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, Response, stream_with_context
+import time
+import json
 
 from mqtt.MQTTsingle import MQTTSingle
 from static.consts import MQTT_TOPIC_CONTROL, MQTT_COMMANDS, GAME_LEVELS
@@ -34,8 +36,29 @@ def game_process():
     
     session.pop('game_level', None)
     
-    mqtt_singleton.publish_message(MQTT_TOPIC_CONTROL, MQTT_COMMANDS.START)
-    return render_template('gameprocess.html', level=level)
+    mqtt_singleton.publish_message(MQTT_COMMANDS.START)
+    return render_template('gameprocess.html', level=level, btns=MQTT_COMMANDS)
+
+@app.route('/stream')
+def stream():
+    def generate():
+        message_count = 0
+        while True:
+            current_messages = mqtt_singleton.get_messages()
+            if len(current_messages) > message_count:
+                new_messages = current_messages[message_count:]
+                message_count = len(current_messages)
+                yield f"data: {json.dumps(new_messages)}\n\n"
+            time.sleep(1)
+
+    return Response(stream_with_context(generate()), mimetype='text/event-stream')
+
+@app.route('/publish', methods=['POST'])
+def publish():
+    command = request.json.get('command')
+    mqtt_singleton.publish_message(command)
+    # TODO: error handling
+    return json.dumps({'status': 'success'})
 
 if __name__ == '__main__':
     app.run()
--- mqtt/MQTTsingle.py
+++ mqtt/MQTTsingle.py
@@ -1,8 +1,9 @@
 import os
 import paho.mqtt.client as mqtt
 import logging
+
 from dotenv import load_dotenv
-from static.consts import LOGFILE
+from static.consts import LOGFILE, MQTT_TOPIC_DATA, MQTT_TOPIC_CONTROL
 
 class MQTTSingle:
     _instance = None
@@ -23,9 +24,12 @@ class MQTTSingle:
         self.__logger = logging.getLogger(__name__)
         self.__logger.setLevel(logging.DEBUG)
 
+        self.__messages = []
+
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.client.on_connect = self.on_connect
         self.client.on_publish = self.on_publish
+        self.client.on_message = self.on_message
         try:
             self.client.username_pw_set(username=os.getenv("USERNAME"),password=os.getenv("PASSWORD"))
             self.client.connect(os.getenv("HOST"), int(os.getenv("PORT")))
@@ -37,6 +41,7 @@ class MQTTSingle:
     def on_connect(self, client, userdata, flags, rc, props):
         self.__logger.info("Connected with result code "+str(rc))
         print("Connected with result code "+str(rc))
+        client.subscribe(MQTT_TOPIC_DATA)
 
     def on_close(self):
         self.client.disconnect()
@@ -45,9 +50,19 @@ class MQTTSingle:
     def on_publish(self, client, userdata, mid, reason_code, properties):
         try:
             self.__logger.info('message published status: '+ str(reason_code))
+            # TODO: error handling
         except KeyError:
             print(KeyError)
 
-    def publish_message(self,TOPIC, msg):
-        msg_info = self.client.publish(TOPIC, msg)
+    def on_message(self, client, userdata, msg):
+        if msg.topic == MQTT_TOPIC_DATA:
+            data = msg.payload.decode()
+            self.__messages.append(data)
+            # TODO: move word/finish distinguishment here?
+    
+    def publish_message(self, msg):
+        msg_info = self.client.publish(MQTT_TOPIC_CONTROL, msg)
         msg_info.wait_for_publish()
+
+    def get_messages(self):
+        return self.__messages
