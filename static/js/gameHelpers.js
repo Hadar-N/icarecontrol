e698bbd004eb79c1ceb625e94e8a4640e6e7280a
Hadar-N <hadarnv1@gmail.com>
2025-04-24 11:54:01 +0800

    cleanout

--- static/js/gameHelpers.js
+++ static/js/gameHelpers.js
@@ -1,7 +1,3 @@
-function updateText(elm_id, str) {
-    document.getElementById(elm_id).textContent = str
-}
-
 function isEnglish(str) {
     return (str && !!str.match(/^[A-Za-z]/))
 }
@@ -56,24 +52,15 @@ function createSpeakerButton(str, add_onclick = true) {
     svg.classList.add('spkr-img')
     svg.id = `sp-${str}`
     if(add_onclick) {
-    svg.onclick = (e) => {
-        e.stopPropagation()
-        speakWord(str)
-    };
+        svg.onclick = (e) => {
+            e.stopPropagation()
+            speakWord(str)
+        };
     }
     return svg;
 }
 
-function createChooseOptionBtn(str) {
-    const div = document.createElement('div');
-    div.id = `optionbtn-${str}`
-    div.textContent = GameState.getStrings()['gameprocess.html'].options_btn
-    div.classList.add('choose-option-btn')
-    div.onclick = () => GameState.changeToggle(str)
-    return div
-}
-
-function createListItem(str, options = []) {
+function createListItem(str) {
     const li = document.createElement('li');
     li.id = `l-${str}`
     const is_english = isEnglish(str)
@@ -83,88 +70,8 @@ function createListItem(str, options = []) {
     if (is_english) {
         div.prepend(createSpeakerButton(str))
     }
-    if (options.length) {
-        div.append(createChooseOptionBtn(str))
-    }
     li.appendChild(div);
     return li;
-}
-
-function messageEffects(msg, list_elm, matched_list, sounds) {
-    const actions = GameState.getConsts().MQTT_DATA_ACTIONS
-    switch (msg.type) {
-        case actions.NEW:
-            if (msg.word.word && typeof msg.word.word === "string" && !GameState.getWord(msg.word.word)) {
-                GameState.addWord(msg.word)
-                list_elm.prepend(createListItem(msg.word.word, msg.word.options));
-                GameState.changeToggle(msg.word.word, true)
-                if (isEnglish(msg.word.word)) speakWord(msg.word.word)
-            }
-            break;
-        case actions.REMOVE:
-            let existing_elm = document.getElementById(`l-${msg.word.word}`)
-            if (GameState.isToggleOpen() && GameState.currToggleWord() == msg.word.word) GameState.changeToggle(msg.word.word, false) // TODO: fix
-            if (existing_elm) existing_elm.remove();
-            GameState.removeWord(msg.word)
-            break;
-        case actions.MATCHED:
-            document.getElementById(`l-${msg.word.word}`).classList.add("matched");
-            if (isEnglish(msg.word.word)) document.getElementById(`sp-${msg.word.word}`).disabled = true; // TODO: fix
-            matched_list.push(msg.word);
-            GameState.changeToggle(msg.word.word, false)
-            sounds.success.play()
-            break;
-        case actions.STATUS:
-            failed_match = GameState.getWord(msg.word.word)?.options.find(orig_op => {
-                let found = msg.word.options.find(curr_opt => curr_opt.word === orig_op.word)
-                return found.is_attempted !== orig_op.is_attempted
-            })
-            if (failed_match) {
-                GameState.addWord(msg.word)
-                if (GameState.isToggleOpen() && GameState.currToggleWord() == msg.word.word) GameState.changeToggle(msg.word.word, true)
-                sounds.fail.play()
-            }
-            break;
-        case actions.SELECT_FAIL:
-            speakWord(GameState.getStrings()["gameprocess.html"].select_fail)
-        default:
-            console.error("type non valid!", msg.type);
-    }
-}
-
-const changePopUpContent = (title_elm, option_list_elm) => (str) => {
-    obj = GameState.getWord(str);
-    title_elm.textContent = obj.word;
-    option_list_elm.innerHTML = "";
-    for (let opt of obj.options) {
-        item_elm = createListItem(opt.word)
-        if (opt.is_attempted) item_elm.classList.add("fail-attempted");
-        else {
-            item_elm.classList.add('not-yet-attempted');
-            item_elm.onclick = () => {
-                if (isEnglish(opt.word)) speakWord(opt.word)
-                fetch('/publish_select', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        word: obj.word,
-                        selected: opt.word
-                    })
-                })
-            }
-        }
-        option_list_elm.append(item_elm)
-    }
-}
-
-function titleBasedOnStatus(status) {
-    let res = '';
-    res = GameState.getStrings()["gameend.html"].title?.[status]
-    if (!res) {
-        console.error('unrecognized game status!', status)
-        res = GameState.getStrings()["gameend.html"].title.error
-    }
-    return res;
 }
 
 function supportListScrolling(div) {
--- static/js/gameState.js
+++ static/js/gameState.js
@@ -6,12 +6,6 @@ const GameState = (function () {
     let consts = {};
     let strings = {};
 
-    let toggled_elm= null;
-    let is_toggle=null;
-    let curr_toggle_word = null;
-    let on_toggle_func = null;
-
-    let words = {}
 
     return {
         getConsts: () => {
@@ -31,36 +25,6 @@ const GameState = (function () {
         },
         setStrings: (str) => {
             strings = parseFromFlaskJson(str)
-        },
-        addWord: (obj) => {
-            words[obj.word] = obj
-        },
-        removeWord: (obj) => {
-            if(words[obj.word]) delete words[obj.word]
-        },
-        getWord: (str) => { return(words[str]) },
-        registerToggledObj: (elm) => {
-            toggled_elm = elm;
-            toggled_elm.style.display = is_toggle ? 'block' : 'none';
-        },
-        registerOnToggleFunc: (func) => {
-            on_toggle_func = func;
-        },
-        isToggleOpen: () => {
-            return is_toggle;
-        },
-        currToggleWord: () => {
-            return curr_toggle_word;
-        },
-        changeToggle: (str, force_toggle = null) => {
-            is_toggle = force_toggle === null ? !is_toggle : force_toggle;
-
-            if(on_toggle_func && str) {
-                on_toggle_func(str)
-                curr_toggle_word = str
-            }
-
-            if(toggled_elm) toggled_elm.style.display = is_toggle ? 'block' : 'none';
         }
     };
 })();
